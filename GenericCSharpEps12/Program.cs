dc125d725b76261c4df8846e21a675fac39216f1
Alifkm <alifkahfi@gmail.com>
2024-10-17 05:53:35 +0700

    feat: first commit

---
+++ GenericCSharpEps12/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+
+namespace GenericCSharpEps12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Type type = typeof(ConfigurationManager<>);
+        }
+    }
+
+    internal class ConfigurationManager<T>
+    {
+        public T LoadedConfiguration { get; set; }
+        public ConfigurationManager(T config) 
+        { 
+            LoadedConfiguration = config;
+        }
+
+        public static void SaveConfig(T configToSave)
+        {
+
+        }
+    }
+}
